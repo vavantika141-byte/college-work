8b7b63105a3732551aab6852ed7af9b098b5d9e6
Avantika verma <vavantika141@gmail.com>
2025-11-05 11:46:49 +0530

    for loop

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    int num;
+    printf("Enter the number:  ");
+    scanf("%d",&num);
+
+    for (; i<=num;)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+}
