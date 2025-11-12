02793b5c1824eb6f7b787a421b53de3433ee49b2
Avantika verma <vavantika141@gmail.com>
2025-11-12 14:56:46 +0530

    pattern

--- pattern.c
+++ pattern.c
@@ -8,19 +8,19 @@ int main()
     scanf("%d",&n);
     for (i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
             printf("#");
         }
         printf("\n");
     }
-}*/
+}
 
-
+// upper triangular pattern 
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int i,j,n;  
     printf("Enter the no. of rows");
     scanf("%d",&n);
     for (i=1;i<=n;i++)
@@ -32,3 +32,26 @@ int main()
         printf("\n");
     }
 }
+*/
+
+//code for upper triangular
+#include<stdio.h>
+int main()
+{
+    int i,j,n,p;  
+    printf("Enter the no. of rows");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        for (p=1;p<=n-i;p++)
+           {
+            printf(" ");
+           }
+        for(j=1;j<=i;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
